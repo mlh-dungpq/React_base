36df0e9cceb0bea1d25c337a04141d937317f39b
huynq1 <internship.hpa@lifetimetech.vn>
2020-12-07 17:38:23 +0700

    Add custom field

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.scss';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import Pokemon from './pokemon/Pokemon';
-import Users from './users/Users';
-import Post from './post/Post';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Pokemon from "./pokemon/Pokemon";
+import Users from "./users/Users";
+import Post from "./post/Post";
+import Login from "./login/Login";
 
 function App() {
   return (
@@ -20,18 +20,24 @@ function App() {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
           </ul>
         </nav>
 
         <Switch>
-          <Route path="/post">
+          <Route path="/post" exact>
             <Post />
           </Route>
-          <Route path="/users">
+          <Route path="/users" exact>
             <Users />
           </Route>
-          <Route path="/">
+          <Route path="/" exact>
             <Pokemon />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
           </Route>
         </Switch>
       </div>
--- src/users/Users.tsx
+++ src/users/Users.tsx
@@ -1,86 +1,91 @@
-import React from 'react';
-import './Users.scss';
-import { Formik, Form, Field } from 'formik';
+import React from "react";
+import "./Users.scss";
+import { Formik, Form, Field } from "formik";
 const yup = require("yup");
 
 const SignupSchema = yup.object().shape({
-  firstName: yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: yup.string()
-    .email('Invalid email')
-    .required('Required'),
-  password: yup.string()
-    .min(8, 'Too Short!')
-    .required('Required'),
-  confirmPassword: yup.string()
-    .oneOf([yup.ref('password'), null], 'Passwords must match')
-    .required('Required'),
+  firstName: yup
+    .string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: yup
+    .string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: yup.string().email("Invalid email").required("Required"),
+  password: yup.string().min(8, "Too Short!").required("Required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
 });
 
 export default function Users() {
   const handleReset = () => {
-    if (!window.confirm('Reset?')) {
-      throw new Error('Cancel reset');
+    if (!window.confirm("Reset?")) {
+      throw new Error("Cancel reset");
     }
   };
 
   return (
-    <> 
-    <h1 className="text-center">Signup</h1>
-    <div className="user">
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={values => {
-        // same shape as initial values
-        console.log(values);
-      }}
-      onReset={handleReset}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <label>First Name</label>
-          <Field name="firstName" type="text" />
-          {errors.firstName && touched.firstName ? (
-            <div className="error">{errors.firstName}</div>
-          ) : null}
+    <>
+      <h1 className="text-center">Signup</h1>
+      <div className="user">
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={(values) => {
+            // same shape as initial values
+            console.log(values);
+          }}
+          onReset={handleReset}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <label>First Name</label>
+              <Field name="firstName" type="text" />
+              {errors.firstName && touched.firstName ? (
+                <div className="error">{errors.firstName}</div>
+              ) : null}
 
-          <label>Last Name</label>
-          <Field name="lastName" type="text" />
-          {errors.lastName && touched.lastName ? (
-            <div className="error">{errors.lastName}</div>
-          ) : null}
+              <label>Last Name</label>
+              <Field name="lastName" type="text" />
+              {errors.lastName && touched.lastName ? (
+                <div className="error">{errors.lastName}</div>
+              ) : null}
 
-          <label>Email</label>
-          <Field name="email" type="email" />
-          {errors.email && touched.email ? <div className="error">{errors.email}</div> : null}
+              <label>Email</label>
+              <Field name="email" type="email" />
+              {errors.email && touched.email ? (
+                <div className="error">{errors.email}</div>
+              ) : null}
 
-          <label>Password</label>
-          <Field name="password" type="password" />
-          {errors.password && touched.password ? <div className="error">{errors.password}</div> : null}
+              <label>Password</label>
+              <Field name="password" type="password" />
+              {errors.password && touched.password ? (
+                <div className="error">{errors.password}</div>
+              ) : null}
 
-          <label>Confirm Password</label>
-          <Field name="confirmPassword" type="password" />
-          {errors.confirmPassword && touched.confirmPassword ? <div className="error">{errors.confirmPassword}</div> : null}
+              <label>Confirm Password</label>
+              <Field name="confirmPassword" type="password" />
+              {errors.confirmPassword && touched.confirmPassword ? (
+                <div className="error">{errors.confirmPassword}</div>
+              ) : null}
 
-          <button type="submit">Submit</button>
-          <button type="reset">Reset</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-  </>
+              <button type="submit">Submit</button>
+              <button type="reset">Reset</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
   );
 }
