d8226fe6f23aaf65b1a23b2def7e2954dd1320b9
mlh-dungpq <dungpq@lifetimetech.vn>
2020-12-10 11:20:15 +0700

    mlh-dungpq add redux persist store

--- src/store.tsx
+++ src/store.tsx
@@ -1,12 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
 import RootReducer from "./_common/state/commonReducers";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(
-  RootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const persistConfig = {
+  key: 'token',
+  storage: storage,
+  whitelist: ['token']
+};
+
+const pReducer = persistReducer(persistConfig, RootReducer);
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(pReducer, middleware);
+const persistor = persistStore(store);
+export { persistor, store };
 
 export type RootStore = ReturnType<typeof RootReducer>;
-export default store;
